254609fd82f8ad2b11aed70fbfd66cf3c9199423
Luis Alfredo Del Castillo Rivera <luis.dcr25@gmail.com>
2019-08-16 08:58:11 -0500

    creating class

--- app/src/main/java/com/example/boletasinfaauthfinal/Clases/Employees.java
+++ app/src/main/java/com/example/boletasinfaauthfinal/Clases/Employees.java
@@ -2,7 +2,58 @@ package com.example.boletasinfaauthfinal.Clases;
 
 public class Employees {
 
+    public String NSA;
+    public String MES;
+    public String ANIO;
     public int ID;
+    public String TELEFONO;
+    public byte [] FILE_BOL;
 
+    public String getNSA() {
+        return NSA;
+    }
 
+    public void setNSA(String NSA) {
+        this.NSA = NSA;
+    }
+
+    public String getMES() {
+        return MES;
+    }
+
+    public void setMES(String MES) {
+        this.MES = MES;
+    }
+
+    public String getANIO() {
+        return ANIO;
+    }
+
+    public void setANIO(String ANIO) {
+        this.ANIO = ANIO;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTELEFONO() {
+        return TELEFONO;
+    }
+
+    public void setTELEFONO(String TELEFONO) {
+        this.TELEFONO = TELEFONO;
+    }
+
+    public byte[] getFILE_BOL() {
+        return FILE_BOL;
+    }
+
+    public void setFILE_BOL(byte[] FILE_BOL) {
+        this.FILE_BOL = FILE_BOL;
+    }
 }
